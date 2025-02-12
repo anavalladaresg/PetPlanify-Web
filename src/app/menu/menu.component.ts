42eead9d8d14c13e1335cdc2a48f2198298497de
Ana <avalladaresgonzalez@danielcastelao.org>
2025-02-12 12:41:00 +0100

    Cambiada barra de navegacion

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,62 +5,29 @@ import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { AvatarModule } from 'primeng/avatar';
 
-
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css'],
     standalone: true,
     imports: [MegaMenu, ButtonModule, CommonModule, AvatarModule]
 })
+
 export class MenuComponent implements OnInit {
-    items: MegaMenuItem[] | undefined;
+    menuItems: MegaMenuItem[] | undefined;
 
     ngOnInit() {
-        this.items = [
+        this.menuItems = [
             {
-                label: 'Company',
-                root: true,
-                items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Featureserbtnferbgtn', icon: 'pi pi-list', subtext: 'Subtext of item' },
-                                { label: 'Customers', icon: 'pi pi-users', subtext: 'Subtext of item' },
-                                { label: 'Case Studies', icon: 'pi pi-file', subtext: 'Subtext of item' }
-                            ]
-                        }
-                    ],
-                    [
-                        {
-                            items: [
-                                { label: 'Solutions', icon: 'pi pi-shield', subtext: 'Subtext of item' },
-                                { label: 'Faq', icon: 'pi pi-question', subtext: 'Subtext of item' },
-                                { label: 'Library', icon: 'pi pi-search', subtext: 'Subtext of item' }
-                            ]
-                        }
-                    ],
-                    [
-                        {
-                            items: [
-                                { label: 'Community', icon: 'pi pi-comments', subtext: 'Subtext of item' },
-                                { label: 'Rewards', icon: 'pi pi-star', subtext: 'Subtext of item' },
-                                { label: 'Investors', icon: 'pi pi-globe', subtext: 'Subtext of item' }
-                            ]
-                        }
-                    ],
-                    [
-                        {
-                            items: [{ image: 'https://primefaces.org/cdn/primeng/images/uikit/uikit-system.png', label: 'GET STARTED', subtext: 'Build spectacular apps in no time.' }]
-                        }
-                    ]
-                ]
+                label: '¿POR QUÉ UNIRTE?',
+                root: true, 
             },
             {
-                label: 'Resources',
+                label: 'PLANES',
                 root: true
             },
             {
-                label: 'Contact',
+                label: 'DESCUBRE MÁS',
                 root: true
             }
         ];
