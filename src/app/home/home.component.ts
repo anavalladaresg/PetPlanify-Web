7ac6bdb74d2011de54125a113786c3f1b13ff983
Ana <avalladaresgonzalez@danielcastelao.org>
2025-02-13 22:49:10 +0100

    transicion tarjetas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  constructor(private el: ElementRef) { }
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); // Agrega la clase cuando es visible
+        }
+      });
+    }, { threshold: 0.2 });
+
+    const tarjetas = this.el.nativeElement.querySelector('.contenedor');
+    if (tarjetas) {
+      observer.observe(tarjetas);
+    }
+  }
 }
