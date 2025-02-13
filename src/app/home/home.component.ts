296e33a46d1fb26dca414efdc8d477d27e57dd27
Ana <avalladaresgonzalez@danielcastelao.org>
2025-02-13 21:03:21 +0100

    añadidas tarjetas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 @Component({
   selector: 'app-home',
-  imports: [AnimateOnScrollModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
 }
