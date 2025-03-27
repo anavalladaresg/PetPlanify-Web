8a46a501f8301a8915ec48b8262927bec5d32976
Ana <avalladaresgonzalez@danielcastelao.org>
2025-03-27 14:23:25 +0100

    añadido fondo al welcome

--- src/app/features/home/pages/welcome/welcome.component.ts
+++ src/app/features/home/pages/welcome/welcome.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  standalone: true,
-  template: `<p>welcome works!</p>`
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
   constructor() {
