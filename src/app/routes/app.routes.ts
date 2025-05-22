7b8f5cd18e72dc640004e1ef42aea19cae490381
anavalladaresg <avalladaresgonzalez@danielcastelao.org>
2025-05-22 11:56:27 +0200

    Añadida la ruta del dashboard al fichero app.routes.ts

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../features/home/home.component';
 import { LoginComponent } from '../features/auth/components/login/login.component';
 import { RegisterComponent } from '../features/auth/components/register/register.component';
+import { DashboardComponent } from '../features/dashboard/components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
   // Aquí puedes añadir más rutas para login, registro, dashboard, etc.
 ];
